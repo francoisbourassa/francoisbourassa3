c15c3ea1b7f113efea3610ee775f311e5cffcef5
francois bourassa <you@example.com>
2023-06-08 09:10:29 -0400

    first commit

---
+++ components/jobdetails/tabs/Tabs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+
+const Tabs = () => {
+  return (
+    <View>
+      <Text>Tabs</Text>
+    </View>
+  )
+}
+
+export default Tabs
---
+++ hook/useFetch_1.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch_1 = (jobDetails, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`your-api-url/${jobDetails}?${query}`);
+        setData(response.data);
+        setError(null);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [jobDetails, query]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch_1;
